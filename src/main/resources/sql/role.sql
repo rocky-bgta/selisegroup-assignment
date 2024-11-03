3c4ba3918c829f12d59cf6433ada4f6f403e5ecf
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2024-11-04 02:24:27 +0600

    data initalization

---
+++ src/main/resources/sql/role.sql
@@ -0,0 +1,2 @@
+INSERT INTO role (role_name, role_fk) VALUES ('ROLE_USER', 1);  -- assuming user with id=1 exists
+INSERT INTO role (role_name, role_fk) VALUES ('ROLE_ADMIN', 2); -- assuming user with id=2 exists
---
+++ src/main/resources/sql/user.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (username, password) VALUES ('john_doe', 'password123');
+INSERT INTO user (username, password) VALUES ('jane_doe', 'password456');
