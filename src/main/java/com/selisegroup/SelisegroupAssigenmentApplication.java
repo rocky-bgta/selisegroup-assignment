c1d7807e71d8a73cc7c4a99d51afb99be2d73b51
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2024-11-03 23:22:43 +0600

    init project

---
+++ src/main/java/com/selisegroup/SelisegroupAssigenmentApplication.java
@@ -0,0 +1,13 @@
+package com.selisegroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelisegroupAssigenmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelisegroupAssigenmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selisegroup/SelisegroupAssigenmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selisegroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelisegroupAssigenmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
